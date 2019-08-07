adc7469bc4211ba34477d9eddb76f0d2f2b2d96f
GodTervsy <atharvanz@gmail.com>
2019-08-08 10:04:17 +1200

    Save Point #1

--- js/mathgame.js
+++ js/mathgame.js
@@ -1,3 +1,30 @@
+let movePlayer, player, movement, Launcher;
+
+movement = {
+    left: false,
+    right: false,
+    fire: false,
+    keyListener: function (event) {
+
+        let key_state = (event.type == "keydown") ? true : false;
+
+        switch (event.keyCode) {
+
+            case 32:
+                controller.fire = key_state;
+                break;
+            case 37:
+                controller.left = key_state;
+                break;
+            case 39:
+                controller.right = key_state;
+                break;
+
+
+        }
+    }
+};
+
 function initCanvas() {
     let ctx = document.getElementById("canvas").getContext("2d")
     let cW = ctx.canvas.width;
@@ -49,7 +76,7 @@ function initCanvas() {
             ctx.fillRect(rectangles[i].x, rectangles[i].y += 1, rectangles[i].w, rectangles[i].h);
             ctx.font = "30px Arial";
             ctx.textAlign = "center";
-            ctx.fillText(answers[i], rectangles[i].x, rectangles[i].y);
+            ctx.fillText("6", rectangles[i].x, rectangles[i].y);
         }
     }
 
@@ -57,12 +84,17 @@ function initCanvas() {
         this.y = 280, this.x = cW * .5 - .25, this.w = 50, this.h = 50, this.dir, this.bg = "orange",
             this.missiles = [];
         this.render = function () {
-            if (this.dir == "left") {
-                this.x -= 5;
-            } else if (this.dir == "right") {
-                this.x += 5;
+            if (movement.left) {
+                alert("left");
+
+                launcher.dir = "left";
+
             }
 
+            if (movement.right) {
+
+                launcher.dir = "right";
+            }
             ctx.fillStyle = this.bg;
             ctx.fillRect(this.x, this.y, this.w, this.h);
             for (let i = 0; i < this.missiles.length; i++) {
@@ -104,7 +136,8 @@ function initCanvas() {
         renderEnemies();
     }
     let animateInterval = setInterval(animate, 30);
-    let left_btn = document.getElementById("left_btn");
+
+    /*let left_btn = document.getElementById("left_btn");
     let right_btn = document.getElementById("right_btn");
     let fire_btn = document.getElementById("fire_btn");
     left_btn.addEventListener("mousedown", function (event) {
@@ -131,11 +164,15 @@ function initCanvas() {
             "h": 10,
             "bg": "red"
         });
-    });
+    });*/
+
+    window.requestAnimationFrame(Launcher);
 }
 
-
-
-window.addEventListener("load", function (event) {
+/*window.addEventListener("load", function (event) {
     initCanvas();
-});
+});*/
+
+window.addEventListener("keydown", movement.keyListener);
+window.addEventListener("keyup", movement.keyListener);
+requestAnimationFrame(Launcher);
